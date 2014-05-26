be994f077bc33e3929380743d1599db0c28fb53b
IanZhang1990 <ian.zhang1990@gmail.com>
2014-05-26 16:03:59 -0400

    fix cost bugs

--- SphereSamplingCar/costfunction.py
+++ SphereSamplingCar/costfunction.py
@@ -4,35 +4,38 @@ import math
 def cost(x,y):
 	startAngl = math.pi/4;
 	goalAngl = 0;
-	length = math.sqrt(x**2 + y**2);
+	length = math.sqrt(x**2+y**2);
 	pathAngl = math.atan2(y,x);
 
-	if( math.fabs(pathAngl) - startAngl == 0 ):
+	if( pathAngl <= (-math.pi/2-(math.pi/2 - startAngl) ) ):
 		return length + math.pi/4;
-	elif( pathAngl < 0 and math.fabs(pathAngl) - startAngl > 0 ):
-		theta = math.fabs(pathAngl) - startAngl;
-		if theta > math.pi/2:
-			theta = math.pi/2 - theta;
+	elif( pathAngl < 0 and pathAngl > (-math.pi/2-(math.pi/2 - startAngl) ) ):
+		if( pathAngl <= -startAngl ):
+			theta = math.pi - math.fabs(pathAngl) - startAngl;
+		else:
+			theta = math.fabs(pathAngl) + startAngl;
+
 		d = length * math.sin( theta );
 		return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + math.pi/4;
-	elif( math.fabs(pathAngl) - startAngl < 0 ):
-		return length + math.pi/4;
 	elif( pathAngl == 0 ):
 		return length + math.pi/4;
-	elif( pathAngl > 0 and math.fabs(pathAngl) - startAngl > 0 ):
-		theta = math.fabs(pathAngl) - startAngl;
-		if theta > math.pi/2:
-			theta = math.pi/2 - theta;
+	elif( pathAngl > 0 and pathAngl <= startAngl ):
+		return length + math.pi/4;
+	elif( pathAngl > startAngl ):
+		if( pathAngl <= startAngl + math.pi/2 ):
+			theta = pathAngl - startAngl;
+		else:
+			theta = math.pi - pathAngl + startAngl;
 		d = length * math.sin( theta );
 		return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + math.pi/4;
 
 def main():
 	data = [];
 
-	for i in range( 0, 40 ):
-		for j in range( 0,40 ):
-			x = float(i) / 20.0;
-			y = float(j) / 20.0;
+	for i in range( -60, 60 ):
+		for j in range( -60,60 ):
+			x = float(i) / 30.0;
+			y = float(j) / 30.0;
 			c = cost( x, y );
 			data.append( (x,y,c) );
 
