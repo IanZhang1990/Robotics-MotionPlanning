98724e32b932da0e92fc6354f7750ff0558f3005
IanZhang1990 <ian.zhang1990@gmail.com>
2014-05-28 00:01:12 -0400

    bug fixed.....

--- SphereSamplingCar/costfunction.py
+++ SphereSamplingCar/costfunction.py
@@ -1,33 +1,43 @@
 
 import math
 
-def cost(x,y):
-	startAngl = math.pi/4;
-	goalAngl = 0;
+def g(phi1, phi2):
+	if( math.fabs(phi1-phi2) < math.pi ):
+		return math.fabs(phi1-phi2);
+	else:
+		return math.pi - math.fabs(phi1-phi2);
+
+def cost(x, y, phi):
+	startAngl = phi; #math.pi/4;
 	length = math.sqrt(x**2+y**2);
 	pathAngl = math.atan2(y,x);
 
-	if( pathAngl <= (-math.pi/2-(math.pi/2 - startAngl) ) ):
-		return length + math.pi/4;
-	elif( pathAngl < 0 and pathAngl > (-math.pi/2-(math.pi/2 - startAngl) ) ):
-		if( pathAngl <= -startAngl ):
+	if( pathAngl <= -math.pi+ startAngl ):
+		return length + g(phi, 0);
+	elif( pathAngl < 0 and pathAngl >= (-math.pi + startAngl) ):
+		if( pathAngl <= startAngl - math.pi/2 ):
 			theta = math.pi - math.fabs(pathAngl) - startAngl;
+			d = length * math.sin( theta ); 
+			return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + g(phi, 0);
 		else:
-			theta = math.fabs(pathAngl) + startAngl;
-
-		d = length * math.sin( theta );
-		return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + math.pi/4;
+			theta = math.pi - startAngl;
+			d = math.fabs(y) / math.sin( theta );
+			return d/2*math.pi + d + math.fabs( x - math.fabs(y)/math.tan(theta) ) + g(phi, 0);
+		
 	elif( pathAngl == 0 ):
-		return length + math.pi/4;
+		return length + g(phi, 0);
 	elif( pathAngl > 0 and pathAngl <= startAngl ):
-		return length + math.pi/4;
+		return length + g(phi, 0);
 	elif( pathAngl > startAngl ):
 		if( pathAngl <= startAngl + math.pi/2 ):
 			theta = pathAngl - startAngl;
+			d = length * math.sin( theta );
+			return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + g(phi, 0);
 		else:
-			theta = math.pi - pathAngl + startAngl;
-		d = length * math.sin( theta );
-		return d/2*math.pi + math.fabs( length*math.cos(theta) - d ) + math.pi/4;
+			theta = math.pi/2 - startAngl;
+			d = y / math.sin(theta);
+			return d/2*math.pi + d + math.fabs( math.fabs(x) - y/math.tan(theta) ) + g(phi, 0);
+		
 
 def main():
 	data = [];
@@ -36,9 +46,9 @@ def main():
 		for j in range( -60,60 ):
 			x = float(i) / 30.0;
 			y = float(j) / 30.0;
-			c = cost( x, y );
+			c = cost( x, y, math.pi/3 );
 			data.append( (x,y,c) );
-
+	
 	f = open('costValue.txt', 'w');
 	f.write("# X\tY\tCost\n");
 	for element in data:
